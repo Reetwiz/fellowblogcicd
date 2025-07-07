a9beeaea7747b0feecf79fb29c09d01b8abde3f3
Reetwiz <ekantakuna001@gmail.com>
2025-07-08 02:03:12 +0545

    fix:  test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/reetwiz-fellowblogs-cd/', 
   plugins: [react()],
+  // This tells Vite to add "/reetwiz-fellowblogs-cd/" before all asset links.
+  base: '/reetwiz-fellowblogs-cd/', 
 })
